2574067feaf818d24dd77a02eaafa696d56fde28
Siqm <rafaelsmello01@gmail.com>
2023-04-12 09:54:25 -0300

    Aula 16 -> Proporção de tela com flex

--- App.js
+++ App.js
@@ -1,30 +1,15 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
-const styles = StyleSheet.create({
-  container: {
-    marginTop:40
-  },
-
-  textoPrincipal: {
-    fontSize: 25,
-    color: 'red'
-  },
-
-  alinhaTexto: {
-    textAlign: 'center'
-  }
-})
-
 class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View style={{flex: 1, backgroundColor:'#ddd'}}>
+        <View style={{ height: 65, backgroundColor: '#121212'}}></View>
 
-        <Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Texto 1</Text>
-        <Text>Texto 2</Text>
-        <Text>Texto 3</Text>
-        <Text style={styles.textoPrincipal}>Texto 4</Text>
+        <View style={{ flex: 1, backgroundColor: '#ddd'}}></View>
+
+        <View style={{ height: 65, backgroundColor: '#121212'}}></View>
       </View>
     )
   }
