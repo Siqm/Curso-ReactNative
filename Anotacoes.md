7b9d6cf5af2d6d1d6f0b65e9fd794a537923e21c
Siqm <rafaelsmello01@gmail.com>
2023-04-19 15:18:39 -0300

    backup

--- Anotacoes.md
+++ Anotacoes.md
@@ -6,3 +6,20 @@
 1. ScrollView -> Habilita o Scroll, indicado para páginas menores 
 2. FlatList -> Indicado para páginas muito grandes, listas muito grandes, altamente perfomatico, só aloca os componentes na tela do usuário
 
+#### Desafio
+
+**Banco React**
+
+* [x] Nome (TextInput)
+* [x] Idade (TextInput)
+
+* [x] Sexo (Picker)
+
+* [ ] Limite conta (Slider)
+
+* [ ] Estudante (Switch)
+
+* [ ] Abrir Conta (Botão)
+
+> Não devem haver dados em branco
+> Mostrar dados na tela após cadastro
--- App.jsx
+++ App.jsx
@@ -1,28 +1,72 @@
-import Slider from '@react-native-community/slider';
+import { Picker } from '@react-native-picker/picker';
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, Switch } from 'react-native';
+import { View, StyleSheet, Text, Switch, TextInput, Button, ImageBackground } from 'react-native';
+import Slider from '@react-native-community/slider'
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      status: true
+      nome: '',
+      idade: '',
+      sexo: 'homem',
+      limite: 50,
+      estudante: false,
     }
   }
 
+  
+
   render() {
+
+    const image = require('./src/components/background/background.png');
 
     return (
       <View style={styles.container}>
-        <Switch 
-          value={this.state.status}
-          onValueChange={ (switchValue) => this.setState({status: switchValue})}
-        />
+        <ImageBackground source={image} resizeMode='stretch' style={styles.image}>
+          <Text>Siq Banco</Text>
 
-        <Text style={{textAlign: 'right', marginRight: 5, fontSize: 30}}>
-          {(this.state.status) ? "True" : "False"}
-        </Text>
+          <View>
+            <TextInput
+              placeholder='Insira seu nome'
+            // onChange={}
+            />
 
+            <TextInput
+              placeholder='Insira sua idade'
+            // onChange={}
+            />
+
+            <Text>Selecione seu sexo:</Text>
+            <Picker
+              selectedValue={this.state.sexo}
+              onValueChange={(valor) => this.setState({ sexo: valor })}
+            >
+              <Picker.Item label='Homem' value='homem' />
+              <Picker.Item label='Mulher' value='mulher' />
+            </Picker>
+
+            <Text>Selecione o limite da conta:</Text>
+            <Slider
+              minimumValue={50}
+              maximumValue={2000}
+              step={100}
+            // onValueChange={}
+            // value={}
+            />
+
+            <Text>Você é estudante?</Text>
+            <Switch
+            // onValueChange={}
+            />
+
+            {/* <Button
+              // onPress={}
+              title='Criar conta'
+              color='#841584'
+            /> */}
+          </View>
+        </ImageBackground>
       </View>
     )
   }
@@ -30,7 +74,10 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    marginTop:20,
+    flex: 1,
   },
+
+  image: {
+    flex: 1,
+  }
 })
